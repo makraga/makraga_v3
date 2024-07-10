a6be0554617fdc131f69078753bb9f3b59786aeb
Makraga <45827261+makraga@users.noreply.github.com>
2024-07-11 00:57:40 +0300

    Add files via upload

---
+++ rules.mk
@@ -0,0 +1,8 @@
+# Build Options
+
+SERIAL_DRIVER = vendor
+WS2812_DRIVER = vendor
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = yes     # Enable keyboard RGB backlight
+ENCODER_ENABLE = yes       # Enables tu use encoders
+LTO_ENABLE = yes
